2cfe6e6abfc434c70107704386a8a618ad32ef06
abhi1616u143-star <abhi1616u143@gmail.com>
2026-01-25 12:08:57 +0530

    Added main class and student object demonstration

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import model.Student;
+
+public class Main{
+    public static void main(String[] args){
+        Student student1 = new Student(1,"Avinash",85);
+        Student student2 = new Student(2,"Ravi",97);
+        student1.display();
+        student2.display();
+    }
+}
